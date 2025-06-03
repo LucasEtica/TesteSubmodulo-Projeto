f4ab39d1257a660c4c8d39cd972ca9da300b52d8
LucasEtica <lucas@etica-ti.com.br>
2025-06-03 08:45:58 -0300

    Projeto para testar a funcionalidade do submodulo do github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesteSubmoduloProjeto::Application.config.secret_token = '77937a2dd1e9918f9daeca19314ea70a7fbe056c7ea9ef73a4896cbf25117b377ac8900a3c1286741ee04c6dc70849067abebdcd6b1a5a200a7db441f6e14c34'

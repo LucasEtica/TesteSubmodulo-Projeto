41eb5d4340f2e7c27c0980207aa0b84f41b404df
LucasEtica <lucas@etica-ti.com.br>
2025-06-03 10:05:46 -0300

    Exclusão do arquivo config para adicionar o modulo que substituirá.

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-TesteSubmoduloProjeto::Application.config.session_store :cookie_store, key: '_teste_submodulo_projeto_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# TesteSubmoduloProjeto::Application.config.session_store :active_record_store
